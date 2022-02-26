537db720f08e2a01999d7a2b0ed14143acc18b6b
d4rwln <darwinop@gmail.com>
2022-02-26 15:15:22 -0300

    firts commit

---
+++ src/entities/Worker.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+	private String name;
+	private WorkerLevel level;
+	private double baseSalary;
+	
+	List<HourContract> contracts = new ArrayList<>();
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public WorkerLevel getLevel() {
+		return level;
+	}
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	
+
+}
